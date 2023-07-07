135433ac10db82ae925d6024c3a311e83cb930dd
Fang Yuan <wojiushifangyuanlove@gmail.com>
2023-07-07 17:04:28 +0800

    tmp: store
    Signed-off-by: Fang Yuan <wojiushifangyuanlove@gmail.com>

---
+++ utilcom/build_test_data.go
@@ -0,0 +1,32 @@
+package utilcom
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func BuildTestArr(num, max, min int) []int {
+	if max < min {
+		panic(" max should be less than min")
+	}
+	rand.Seed(time.Now().UnixNano())
+	//构建随机[]int 最大值 63
+	arr := make([]int, num)
+	for i := 0; i < num; i++ {
+		randNum := rand.Intn(max-min) + min
+		arr[i] = randNum
+	}
+	return arr
+}
+
+func PrintSortComparison(arr []int, f func(arr []int)) {
+	//排序前
+	fmt.Println(arr)
+	fmt.Println(len(arr))
+	//排序
+	f(arr)
+	//排序后
+	fmt.Println(arr)
+	fmt.Println(len(arr))
+}
